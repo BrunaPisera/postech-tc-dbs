c1296218050d77086211c592de29a0fb6e3f3b54
Alexis Cesar <alexis.cesar2017@outlook.com>
2025-02-11 11:54:12 -0300

    Add Atlas access list and user

--- mongodb.tf
+++ mongodb.tf
@@ -11,6 +11,23 @@ resource "mongodbatlas_cluster" "tc_atlas_cluster" {
   provider_region_name         = var.atlas_cluster_region
 }
 
+resource "mongodbatlas_project_ip_access_list" "access_list" {
+  project_id = var.atlas_project_id
+  cidr_block = "0.0.0.0/0"
+}
+
+resource "mongodbatlas_database_user" "user" {
+  username           = var.atlas_mongo_db_username
+  password           = var.atlas_mongo_db_password
+  project_id         = var.atlas_project_id
+  auth_database_name = "admin"
+
+  roles {
+    role_name     = "readWriteAnyDatabase"
+    database_name = "admin"
+  }
+}
+
 output "connection_strings" {
   value = ["${mongodbatlas_cluster.tc_atlas_cluster.connection_strings}"]
 }

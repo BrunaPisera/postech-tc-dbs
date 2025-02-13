3a14c73649ae2c1f643ea2a0a8733bcda46f1beb
bruna.goncalves <pissara2@gmail.com>
2025-02-13 11:20:36 -0300

    Add dbs and aws config

--- 1_variables.tf
+++ 1_variables.tf
@@ -40,3 +40,11 @@ variable "atlas_mongo_db_password" {
   description = "MongoDB user password"
   type        = string
 }
+
+variable "cidrBlocks" {
+  default = "10.0.0.0/16"
+}
+
+variable "defaultRegion" {
+  default = "us-east-1"
+}
